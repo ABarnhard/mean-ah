a6c297abd642519ffc2297328327dfe77de07168
Adam Barnhard <admbarn@gmail.com>
2014-10-09 12:27:07 -0500

    added template files for modals

--- client/views/card-display/card-display.js
+++ client/views/card-display/card-display.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
   angular.module('mean-ah')
-  .controller('CardsDisplayCtrl', ['$scope', function($scope){
+  .controller('CardsDispCtrl', ['$scope', function($scope){
 
   }]);
 })();
