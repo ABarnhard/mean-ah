e440b7b97e4adfe827dc0ddb1a390eeed3f58e44
Adam Barnhard <admbarn@gmail.com>
2014-10-11 20:17:26 -0500

    added player directive

---
+++ client/components/directives/ab-player/ab-player.js
@@ -0,0 +1,25 @@
+(function(){
+  'use strict';
+
+  angular.module('abPlayerModule', [])
+  .directive('abPlayer', [function(){
+    var o = {};
+
+    o.restrict = 'A';
+    o.templateUrl = '/components/directives/ab-player/ab-player.html';
+
+    o.scope = {player:'@', playedRound:'@'};
+
+    o.controller = ['$scope', function($scope){
+      $scope.hasPlayed = false;
+
+      $scope.$watch('playedRound', function(newVal, oldVal){
+        $scope.hasPlayed = !!$scope.playedRound;
+      });
+
+    }];
+
+    return o;
+  }]);
+
+})();
--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
 
-  angular.module('mean-ah', ['ngRoute', 'LocalForageModule'])
+  angular.module('mean-ah', ['ngRoute', 'LocalForageModule', 'abPlayerModule'])
   .config(['$routeProvider', '$httpProvider', '$localForageProvider', function($routeProvider, $httpProvider, $localForageProvider){
     $routeProvider
     .when('/',         {templateUrl:'/views/home/home.html',         controller:'HomeCtrl'})
